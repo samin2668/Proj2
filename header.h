d8243b3fc8ff1a4e83c0125b6db81d173f1bf721
samin2668 <samin2668@gmail.com>
2022-04-28 12:29:05 -0500

    more comments

--- header.h
+++ header.h
@@ -6,21 +6,24 @@
 #include <vector>
 using namespace std;
 
+//function to parse input file
 string parseFile(string fileName, int &p, int &r, string &units);
+
+//class for resource allocation graph
 class Graph {
     private:
-        bool **adjMatrix;
-        int vertices;
-        int processes;
-        int resources;
-        int *resourceUnits;
+        bool **adjMatrix;   //RAG as adjacency matrix - true = edge, false = no edge
+        int vertices;   //number of vertices
+        int processes;  //number of processes
+        int resources;  //number of resources
+        int *resourceUnits; //array to hold available units for each resource
     public:
-        Graph(int p, int r, string units);
-        ~Graph();
-        bool isDeadlock;
-        void printGraph();
-        void printUnits();
-        void inputMatrix(string matrix);
-        void deadlockDetection();
-        string printState();
+        Graph(int p, int r, string units);  //constructor
+        ~Graph();   //destructor
+        bool isDeadlock;    //true = deadlock present, false = no deadlock present
+        void printGraph();  //function to print graph for debugging
+        void printUnits();  //function to print resource array for debugging
+        void inputMatrix(string matrix);    //function to set adjacency matrix from a string parsed from the input file
+        void deadlockDetection();   //finding deadlocks
+        string printState();    //printing current state -> deadlock/no deadlock
 };
